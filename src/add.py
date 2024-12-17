a34e3b9c74110b708718978f3d3c8d1156984865
ChandraSekhar <115386409+chandhu005@users.noreply.github.com>
2024-12-17 22:24:39 +0530

    Update add.py

--- src/add.py
+++ src/add.py
@@ -1,7 +1,7 @@
-def add(a,b):
-    
-    return a+b
+def add(a, b):
+    return a + b
 
-def add_test():
-    assert add(1,2)==8
-    assert add(3,4)==7
+# Correctly named test function
+def test_add():
+    assert add(1, 2) == 3  # Correct expected value
+    assert add(3, 4) == 7  # Correct expected value
